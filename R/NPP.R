c56a4089fd9145488a58fdc2425d9161d4bee32e
benjones13 <ben.jones@plymouth.ac.uk>
2022-01-25 08:21:45 +0000

    Adding parameter checks to NPP

--- R/NPP.R
+++ R/NPP.R
@@ -3,21 +3,26 @@
 #' NPP is used to fit a Normalised Power Prior (NPP) to analysis of a (crruent) dataset, using a second (historical) dataset to formulate the power prior.
 #' @param Trt A vector of 1's and 0's denoting treatment or control arms, respectively, for the current dataset
 #' @param Trt0 A vector of 1's and 0's denoting treatment or control arms, respectivly, for the historical dataset
-#' @param X The design matrix for the current dataset, excluding treatment allocation indicator and intercept term
-#' @param X0 The design matrix for the historical dataset, excluding treatment allocation indicator and intercept term
+#' @param X A matrix. The design matrix for the current dataset, excluding treatment allocation indicator and intercept term
+#' @param X0 A matrix. The design matrix for the historical dataset, excluding treatment allocation indicator and intercept term
 #' @param Y A vector containing the outcome data for the current dataset
+#' @param Y0 A vector containing the outcome data for the current dataset
+#' @param Z A vector of consecutive integers containing cluster indices for the current dataset.
+#' @param Z0 A vector of consecutive integers containing cluster indices for the historical dataset. 
 #' @param sigmaprior One of either "hnormal" or "hcauchy" to indicated whethere a half-normal or half-cauchy prior distribution should be fitted to the between-cluster SD parameter
 #' @param reg.prior.mean The mean for the normal prior distribution for each of the regression coefficients
 #' @param reg.prior.sd The standard deviation for the normal prior distribution for each of the regression coefficients
 #' @param nits_normalise An integer. Number of iterations per chain used in the Markov Chain Monte Carlo procedure for estimating the normalising constant
 #' @param burnin_normalise An integer. Number of iterations per chain to be discarded in the Markov Chain Monte Carlo procedure for estimating the normalising constant
 #' @param nchains_normalise An integer. Number of chains to be used in the Markov Chain Monte Carlo procedure for estimating the normalising constant
-#' @param max_treedepth_normalise Maximum treedepth for the Markov Chain monte carlo procedure for estimating the normalising constant. See link stan documentation
+#' @param max_treedepth_normalise Maximum treedepth for the Markov Chain Monte Carlo procedure for estimating the normalising constant. See link stan documentation
+#' @param thin_normalise A positive integer specifying the period for saving Markov Chain Monte Carlo samples for the procedure estimating the normalising constant. Defaults to 1
 #' @param adapt_delta_normalise Value of adapt delta used in the Markov Chain Monte Carlo procedure for estimating the normalising constant. See link stan documentation
 #' @param nits An integer. Number of iterations per chain to be used in the Markov Chain Monte Carlo procedure for fitting the NPP model
 #' @param burnin An integer. Number of iterations per chain to be discarded in the Markov Chain Monte Carlo procedure for fitting the NPP model
 #' @param nchains An integer. Number of chains to be used in the Markov Chain Monte Carlo procedure for estimating the normalising constant
 #' @param max_treedepth Maximum treedepth for the Markov Chain monte carlo procedure for estimating the NPP. See link stan documentation
+#' @param thin A positive integer specifying the period for saving Markov Chain Monte Carlo samples for the procedure fitting the NPP model. Defaults to 1
 #' @param adapt_delta Value of adapt delta used in the Markov Chain Monte Carlo procedure for estimating the NPP. See link stan documentation
 #' @param ... Further arguments passed to or from other methods
 #' @return TO UPDATE
@@ -26,23 +31,155 @@
 #' @export
 NPP = function(Trt,
                Trt0, 
-               X, 
-               X0, 
+               X = NULL, 
+               X0 = NULL, 
                Y, 
                Y0, 
+               Z, 
+               Z0,
                sigmaprior = c("hnormal", "hcauchy"), 
                reg.prior.mean = 0,
                reg.prior.sd,
                nits_normalise = 2000,
-               burnin_normalise = nits_normalise/2,
+               burnin_normalise = floor(nits_normalise/2),
                nchains_normalise = 4,
                max_treedepth_normalise = 10,
+               thin_normalise = 1,
                adapt_delta_normalise = 0.95,
                nits = 5000,
-               burnin = nits/2,
+               burnin = floor(nits_normalise/2),
                nchains = 4,
                max_treedepth = 10,
+               thin = 1,
                adapt_delta = 0.95,
                ...){
+  ##QA Checks of input parameters##
+  #Trt
+  if(min(Trt %in% c(0,1)) == 0){
+    stop("Values of Trt must be only be 1 or 0")
+  }
+  if(!is.vector(Trt)){
+    stop("Trt must be a vector")
+  }
+  
+  #Trt0
+  if(min(Trt0 %in% c(0,1)) == 0){
+    stop("Values of Trt0 must be only be 1 or 0")
+  }
+  if(!is.vector(Trt0)){
+    stop("Trt0 must be a vector")
+  }
+  
+  #X
+  if(!is.matrix(X)){
+    stop("X must be a matrix")
+  }
+  
+  #X0
+  if(!is.matrix(X0)){
+    stop("X0 must be a matrix")
+  }
+  
+  #Y
+  if(!is.vector(Y)){
+    stop("Y must be a vector")
+  }
+  #Y0
+  if(!is.vector(Y0)){
+    stop("Y must be a vector")
+  }
+  
+  #Z
+  if(!is.vector(Z)){
+    stop("Z must be a vector")
+  }
+  if(length(unique(Z)) != max(Z)){
+    stop(paste0("Cluster labels in Z must be consecutive: every number between 1 and ",max(Z), " must be contained within Z"))
+  }
+  if(min(Z == round(Z)) == 0){
+    stop("All values of Z must be integers")
+  }
+  
+  #Z0
+  if(!is.vector(Z0)){
+    stop("Z0 must be a vector")
+  }
+  if(length(unique(Z0)) != max(Z0)){
+    stop(paste0("Cluster labels in Z0 must be consecutive: every number between 1 and ",max(Z0), " must be contained within Z0"))
+  }
+  if(min(Z0 == round(Z0)) == 0){
+    stop("All values of Z0 must be integers")
+  }
+  #Cross-check datasets - make sure that each dataset has the same sample size across all inputs
+  
+  if(!all(sapply(list(length(Trt0), nrow(X0), length(Y0), length(Z0)), function(x) x == length(Z0)))){
+    stop(cat(paste("All inputs pertaining to the historical data (Trt0, X0, Y0 and Z0) must all contain the same number of elements \n", 
+                   "Trt0 contains ", length(Trt0), " elements. \n",
+                   "X0 contains ", nrow(X0), " elements (rows). \n",
+                   "Y0 contains ", length(Y0), " elements. \n", 
+                   "Z0 contains ", length(Z0), " elements.")))
+  }
+  
+  if(!all(sapply(list(length(Trt), nrow(X), length(Y), length(Z)), function(x) x == length(Z)))){
+    stop(cat(paste("All inputs pertaining to the current data (Trt, X, Y and Z) must all contain the same number of elements \n", 
+                   "Trt contains ", length(Trt), " elements. \n",
+                   "X contains ", nrow(X), " elements (rows). \n",
+                   "Y contains ", length(Y), " elements. \n", 
+                   "Z contains ", length(Z), " elements.")))
+  }
+  
+  if(identical(sigmaprior, c("hnormal", "hcauchy"))){
+    sigmaprior = ifelse(length(unique(Z0)) < 5, "hcauchy", "hnormal")
+  }
+  
+  #Checking Stan inputs
+  
+  if(burnin_normalise >= nits_normalise){
+    stop("burnin_normalise must be less than nits_normalise")
+  }
+  if(adapt_delta_normalise >= 1 | adapt_delta_normalise <= 0){
+    stop("adapt_delta_normalise must be between 0 and 1")
+  }
+  if(max_treedepth_normalise != round(max_treedepth_normalise)){
+    stop("max_treedepth_normalise must be an integer")
+  }
+  if(max_treedepth_normalise <= 0){
+    stop("max_treedepth_normalise must be positive")
+  }
+  if(thin_normalise != round(thin_normalise)){
+    stop("thin_normalise must be an integer")
+  }
+  if(thin_normalise <= 0){
+    stop("thin_normalise must be positive")
+  }
+  
+  
+  if(burnin >= nits){
+    stop("burnin must be less than nits")
+  }
+  if(adapt_delta >= 1 | adapt_delta <= 0){
+    stop("adapt_delta must be between 0 and 1")
+  }
+  if(max_treedepth != round(max_treedepth)){
+    stop("max_treedepth must be an integer")
+  }
+  if(max_treedepth <= 0){
+    stop("max_treedepth must be positive")
+  }
+  if(thin != round(thin)){
+    stop("thin must be an integer")
+  }
+  if(thin <= 0){
+    stop("thin must be positive")
+  }
+  
+  
+  
+  
+  ##Normalisation Approximation
+  
+  
+  ##NPP model fit
+  
   
 }
